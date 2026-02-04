adc04bc9df910307c5efa3785587d48fde400a01
pedrohcfernandes1-cmd <pedro.hcfernandes1@gmail.com>
2026-02-04 20:59:08 -0300

    Primeiro Teste Oficial

--- index.js
+++ index.js
@@ -1,2 +1,37 @@
-let nome = "Pedro"
-console.log(nome);
+let nomeHeroi = "Dante"
+let xp = 15.000
+
+do {
+    xp <= 1.000
+    xp <= 2.000
+     or >= 1.001
+
+    xp <= 3.000
+     or >= 2.001
+
+    xp <= 4.000
+     or >= 3.001
+
+    xp <= 5.000
+     or >= 4.001
+
+    xp <= 6.000
+     or >= 5.001
+
+    xp <= 7.000
+     or >= 6.001
+
+    xp <= 8.000
+     or >= 7.001
+
+    xp <= 9.000
+     or >= 8.001
+
+    xp <= 10.000
+     or >= 9.001
+
+    xp >= 10.001
+
+} while (xp);
+
+console.log(xp)
