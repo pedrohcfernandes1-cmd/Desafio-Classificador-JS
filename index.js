4302a4897f77424f34e4995fdbb74365f663710f
pedrohcfernandes1-cmd <pedro.hcfernandes1@gmail.com>
2026-02-04 21:33:30 -0300

    Revisado os operadores comparacionais e concatenação arrumada também.

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
-// Variáveis do herói,
+// Variáveis
 let nomeHeroi = "Samantha"
-let xpHeroi = 12000
-let nivelHeroi = ""
+let xpHeroi = 800
+let nivel = ""
 
-// Estrutura de decisão para classificar o nível
-if (xpHeroi < 1000) {
-    nivel = "Ferro";
+// Estrutura de decisão 
+if (xpHeroi <= 1000) {
+    nivel = "Ferro"
 } else if (xpHeroi >= 1001 && xpHeroi <= 2000) {
-    nivel = "Bronze";
+    nivel = "Bronze"
 } else if (xpHeroi >= 2001 && xpHeroi <= 5000) {
-    nivel = "Prata";
+    nivel = "Prata"
 } else if (xpHeroi >= 5001 && xpHeroi <= 7000) {
-    nivel = "Ouro";
+    nivel = "Ouro"
 } else if (xpHeroi >= 7001 && xpHeroi <= 8000) {
-    nivel = "Platina";
+    nivel = "Platina"
 } else if (xpHeroi >= 8001 && xpHeroi <= 9000) {
-    nivel = "Ascendente";
+    nivel = "Ascendente"
 } else if (xpHeroi >= 9001 && xpHeroi <= 10000) {
-    nivel = "Imortal";
+    nivel = "Imortal"
 } else {
-    nivel = "Radiante";
+    nivel = "Radiante"
 }
 
-
-// Saída final
-console.log("O Herói de nome " + nomeHeroi + "está no nível de " + nivelHeroi);
+// Saída
+console.log("O Herói de nome " + (nomeHeroi) + " está no nivel " + (nivel) )
